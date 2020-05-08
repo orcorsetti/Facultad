4674448cc49e3930348b4d441d34960e9fa6590c
Ornela Cosetti <orcorsetti@gmail.com>
2020-05-08 18:38:58 -0300

    TTADs Practica EjemploAngular

--- TTADs/Practica/Angular/Ejemplo Clase/testApp-clase2/src/app/app.component.ts
+++ TTADs/Practica/Angular/Ejemplo Clase/testApp-clase2/src/app/app.component.ts
@@ -6,23 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'prueba-clase2';
-  value = 0;
-  list = ['JavaScript', 'TypeScript', 'Java', 'C#']
+  list = []
+  listCompleted = []
+  focus = false
 
-  add(increment) {
-    this.value += increment
-  }
-
-  addElement(){
-    this.list.push('NuevoElemento')
-  }
-
-  getClassesforDiv(){
-    if(this.value > 5){
-      return['resaltado']
+  add(inputControl) {
+    if (inputControl.value === ''){
+      alert('Ingrese un elemento para agregar')
+      return;
     }
-    return['atenuado']
+    this.list.push(
+      {
+      description: inputControl.value,
+      completed: false
+    })
+    inputControl.value = ''
   }
 
+  remove(item) {
+    if (item.completed){
+      const index = this.listCompleted.findIndex(each => each.description===item.description)
+      this.listCompleted.splice(index, 1)
+    } else {
+      const index = this.list.findIndex(each => item.description===each.description)
+      this.list.splice(index,1)
+    }
+  }
+
+  checked(item) {
+    item.completed = true
+    this.listCompleted.push(item)
+    const index = this.list.findIndex(each => item.description===each.description)
+    this.list.splice(index,1)
+  }
+
+  unchecked(item){
+    item.completed = false
+    this.list.push(item)
+    const index = this.listCompleted.findIndex(each => item.description===each.description)
+    this.listCompleted.splice(index,1)
+  }
 }
